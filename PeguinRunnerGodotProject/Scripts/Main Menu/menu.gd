d4e7b638b90f71b48e5aaab256447a0e72762f73
Ace <129903373+dh632@users.noreply.github.com>
2024-03-06 22:49:15 +0000

    Cleaning up menu.gd

--- PeguinRunnerGodotProject/Scripts/Main Menu/menu.gd
+++ PeguinRunnerGodotProject/Scripts/Main Menu/menu.gd
@@ -18,34 +18,7 @@ const WORLD = preload("res://Scenes/world.tscn")
 func _ready():
 	if OS.has_feature('web'):
 		
-		# Get the current directory
-		var current_dir = OS.get_executable_path().get_base_dir()
-		print("Current Directory: ", current_dir)
-
-		# Execute a Python file
-		var python_file_path = "hello.py"
-		
 		# Define the JavaScript code.
-		
-		
-		#var returnVal = js.eval(js_code)
-		#print(returnVal)
-		#js.create_callback(_on_JavaScriptBridge_message_received)
-		
-		var originalBrdige = """
-		var godotBridge = {
-		callback: null,
-		setCallback: (cb) => this.callback = cb,
-		test: (data) => this.callback(JSON.stringify(data)),
-		makeRequest: () => fetch('index.pck')
-			.then(response => response.text())
-			.then(data => {
-				// Send the data back to GDScript.
-				this.callback(data);
-			})
-			.catch(error => console.error('Error:', error))
-		};
-		"""
 		
 		
 		JavaScriptBridge.eval("""
@@ -71,18 +44,6 @@ func _ready():
 		godot_bridge.setCallback(my_callback)
 		godot_bridge.makeRequest()
 		
-		
-		var js_code = """
-		fetch('index.pck')
-			.then(response => response.text())
-			.then(data => {
-				// Send the data back to GDScript.
-				godot_bridge.test(data);
-			})
-			.catch(error => console.error('Error:', error));
-		"""
-		#js.eval(js_code)
-		
 	else:
 		print("The JavaScriptBridge singleton is NOT available")
 		
@@ -91,11 +52,7 @@ func _ready():
 
 func _on_my_callback(args) -> void:
 	print(args)
-		
-
-# This method will be called when the JavaScript code sends a message to GDScript.
-func _on_JavaScriptBridge_message_received(message):
-	print("Received message from JavaScript: ", message)
+	
 
 
 # When the "Start Game" button is pressed, 
