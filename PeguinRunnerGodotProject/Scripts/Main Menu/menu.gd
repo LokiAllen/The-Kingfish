17c874242d0ad2b5aaaecd2556f13b0daa622abf
Ace <129903373+dh632@users.noreply.github.com>
2024-03-10 23:30:36 +0000

    POST requests to the API now work
    Token hardcoded, needs to be undone

--- PeguinRunnerGodotProject/Scripts/Main Menu/menu.gd
+++ PeguinRunnerGodotProject/Scripts/Main Menu/menu.gd
@@ -11,6 +11,7 @@ Menu script for the Main Menu Scene
 
 var js = JavaScriptBridge
 var my_callback = JavaScriptBridge.create_callback(_on_my_callback)
+var django_bridge : JavaScriptObject
 
 # Reference to the world scene
 const WORLD = preload("res://Scenes/world.tscn")
@@ -22,15 +23,12 @@ func _ready():
 		
 		
 		JavaScriptBridge.eval("""
-		var godotBridge = {
+		var djangoBridge = {
 			callback: null,
 			setCallback: function(cb) {
 				this.callback = cb;
 			},
-			test: function(data) {
-				this.callback(JSON.stringify(data));
-			},
-			makeRequest: function() {
+			getUserData: function() {
 				fetch('data/user')
 					.then(response => response.text())
 					.then(data => {
@@ -38,21 +36,49 @@ func _ready():
 						this.callback(JSON.stringify(data));
 					})
 					.catch(error => console.error('Error:', error));
+			},
+			setUserData: function(jsonbody) {
+				fetch('data/user/', {
+						method: "POST", 
+						credentials: 'same-origin',
+						headers: {
+							'Content-Type': 'application/json',
+							'X-CSRFToken': "MwPgaUTEi49bSl5IGx4EkyueGV9mBKel",
+						},
+						body: JSON.stringify({
+							"coins": 0,
+							"highscore": 0,
+							"cumulativeScore": 0,
+						})
+					})
+					.then(response => response.text())
+					.then(data => {})
+					.catch(error => console.error('Error:', error));
 			}
 		};""", true)
-		var godot_bridge = JavaScriptBridge.get_interface("godotBridge")
-		godot_bridge.setCallback(my_callback)
-		godot_bridge.makeRequest()
+		django_bridge = JavaScriptBridge.get_interface("djangoBridge")
+		django_bridge.setCallback(my_callback)
+		django_bridge.getUserData()
+		
 		
 	else:
 		print("The JavaScriptBridge singleton is NOT available")
-		
-		
 
 
 func _on_my_callback(args) -> void:
-	print(args)
+	var jsonParser = JSON.new()
+	print(jsonParser.parse_string(args[0]))
 	
+	jsonParser.parse(args[0])
+	var userInfo = jsonParser.data
+	print(typeof(userInfo))
+	print(userInfo)
+	
+	JavaScriptBridge.eval("console.log(document.cookie)")
+	django_bridge.setUserData(jsonParser.parse_string(args[0]))
+	
+
+
 
 
 # When the "Start Game" button is pressed, 
